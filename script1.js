844c94176159b04aad11577c5a2748f68745c9a3
psuryasnata <155527961+psuryasnata@users.noreply.github.com>
2025-03-16 10:10:36 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,26 +1,35 @@
-// var tl = gsap.timeline()
+var tl = gsap.timeline()
 
-// tl.from("h2",{
-//     y:-20,
-//     opacity: 0,
-//     duration: 0.5,
-//     delay:0.5
-// })
+tl.from("h2",{
+    y:-20,
+    opacity: 0,
+    duration: 0.5,
+    delay:0.5
+})
 
-// tl.from("h4",{
-//     y:-20,
-//     opacity: 0,
-//     duration: 0.3,
-//     delay:0.3,
-//     stagger: 0.3
-// })
+tl.from("h4",{
+    y:-20,
+    opacity: 0,
+    duration: 0.3,
+    delay:0.3,
+    stagger: 0.3
+})
 
-// tl.from(".para",{
-//     x:-20,
-//     opacity: 0,
-//     duration: 0.5,
-//     stagger: 0.3
-// })
+var t2 = gsap.timeline()
+
+t2.from(".para",{
+    x:-20,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5
+})
+
+gsap.from(".image-sec",{
+    x:20,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5
+})
 
 gsap.to(".footer-name h1",{
     transform: "translateX(-89%)",
@@ -34,6 +43,8 @@ gsap.to(".footer-name h1",{
         pin: true
     }
 })
+
+
 
 
 
